6a74aaea588144b8e3687e364e9ae050574d8abf
Joe Connolly <jcjconnolly4@gmail.com>
2019-10-07 22:48:45 -0400

    seemed to have gotten finding the nearest college logic to work

--- app/src/main/java/com/artfara/apps/kipper/SelectCollegeActivity.java
+++ app/src/main/java/com/artfara/apps/kipper/SelectCollegeActivity.java
@@ -1,7 +1,10 @@
 package com.artfara.apps.kipper;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -39,145 +42,135 @@ public class SelectCollegeActivity extends AppCompatActivity {
     private FusedLocationProviderClient mFusedLocationClient;
     private Location mLocation;
     private SharedPreferences mPrefs;
+    private boolean mLocationDialogVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         setContentView(R.layout.activity_select_college);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestLocationAndStartApp();
-        }
 
         mPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                            @Override
-                            public void onSuccess(Location location) {
-                                // Got last known location. In some rare situations this can be null.
-                                if (location != null) {
-                                    mLocation = location;
-                                    // Logic to handle location object
-                                    FirebaseDatabase.getInstance().getReference()
-                                            .child(Globals.COLLEGES_TABLE_NAME)
-                                            .addListenerForSingleValueEvent(
-                                                    new ValueEventListener() {
-                                                        @Override
-                                                        public void onDataChange(DataSnapshot dataSnapshot) {
-                                                            College closestCollege = new College();
-                                                            float minDistance = -1;
-                                                            for (DataSnapshot collegeSnapshot : dataSnapshot.getChildren()) {
-                                                                College college = collegeSnapshot.getValue(College.class);
-                                                                float[] distance = new float[3];
-                                                                Location.distanceBetween(mLocation.getLatitude(),
-                                                                        mLocation.getLongitude(),
-                                                                        college.latitude,
-                                                                        college.longitude, distance);
-                                                                if(minDistance == -1 || distance[0] < minDistance){
-                                                                    closestCollege = college;
-                                                                    minDistance = distance[0];
-                                                                }
-                                                            }
+        findCollege();
+    }
 
-                                                            closestCollege.databaseRoot = closestCollege.databaseRoot + "/";
-                                                            Gson gson = new Gson();
-                                                            mPrefs.edit().putString(Constants.COLLEGE_KEY,
-                                                                    gson.toJson(closestCollege)).apply();
-                                                            Log.d(TAG, "college closest " + closestCollege.name);
-                                                            finish();
-                                                            startActivity(getIntent());
-                                                        }
-
-                                                        @Override
-                                                        public void onCancelled(DatabaseError databaseError) {
-                                                            Log.e(TAG, databaseError.getMessage());
-                                                        }
-                                                    }
-                                            );
-                                }
+    private void showDialog() {
+        mLocationDialogVisible = true;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.location_explanation_message)
+                .setCancelable(false)
+                .setPositiveButton(
+                        "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                mLocationDialogVisible = false;
+                                dialog.dismiss();
+                                findCollege();
                             }
+                        })
+                .create()
+                .show();
+        findCollege();
+    }
+
+    public void requestLocationAndStartApp() {
+        //Only start app if we have the permissions we need to access location
+        if (Build.VERSION.SDK_INT >= 22 && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //We need to request permission
+            if (!mLocationDialogVisible) {
+//                Log.d(TAG, "requesting permission");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        0);
+            }
+        } else {
+            //No need to ask for permission
+            findCollege();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            //permission was granted, yay!
+            //Start tracking users location
+            findCollege();
+
+        } else {
+            // permission denied, boo!
+            // Tell the user that they are a jackass for disabling the permission
+            // must request the permission.
+            showDialog();
+//            Log.d(TAG, "permission denied");
+            //When user presses OK in dialog, onStop() is executed and MapsActivity is restarted, calling onCreate()
+        }
+        return;
     }
 
 
-
-
-
-        public void requestLocationAndStartApp() {
-            //Only start app if we have the permissions we need to access location
-            if (Build.VERSION.SDK_INT >= 22 && ContextCompat.checkSelfPermission(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-                //We need to request permission
-                if (!mLocationDialogVisible) {
-//                Log.d(TAG, "requesting permission");
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                            0);
-                }
-            } else {
-                //No need to ask for permission
-                initializeApplication();
-            }
+    public void findCollege() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationAndStartApp();
+            return;
         }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            mLocation = location;
+                            // Logic to handle location object
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child(Globals.COLLEGES_TABLE_NAME)
+                                    .addListenerForSingleValueEvent(
+                                            new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                                    College closestCollege = new College();
+                                                    float minDistance = -1;
+                                                    for (DataSnapshot collegeSnapshot : dataSnapshot.getChildren()) {
+                                                        College college = collegeSnapshot.getValue(College.class);
+                                                        float[] distance = new float[3];
+                                                        Location.distanceBetween(mLocation.getLatitude(),
+                                                                mLocation.getLongitude(),
+                                                                college.latitude,
+                                                                college.longitude, distance);
+                                                        if (minDistance == -1 || distance[0] < minDistance) {
+                                                            closestCollege = college;
+                                                            minDistance = distance[0];
+                                                        }
+                                                    }
 
+                                                    closestCollege.databaseRoot = closestCollege.databaseRoot + "/";
+                                                    Gson gson = new Gson();
+                                                    mPrefs.edit().putString(Constants.COLLEGE_KEY,
+                                                            gson.toJson(closestCollege)).apply();
+                                                    Log.d(TAG, "college closest " + closestCollege.name);
+                                                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                                                    startActivity(intent);
+                                                    finish();
+                                                }
 
-
-
-
-
-
-//    private ValueEventListener mCollegesSingleEventListener = new ValueEventListener() {
-//        @Override
-//        public void onDataChange(DataSnapshot dataSnapshot) {
-//            mColleges = new ArrayList<>();
-//            for (DataSnapshot collegeSnapshot : dataSnapshot.getChildren()) {
-//                College college = collegeSnapshot.getValue(College.class);
-//                mColleges.add(college);
-////                Log.d(TAG, "college " + college.name);
-//            }
-//        }
-//
-//        @Override
-//        public void onCancelled(DatabaseError databaseError) {
-//            Log.e(TAG, databaseError.getMessage());
-//        }
-//    };
-//
-////    @Override
-//    public void onResume() {
-//        super.onResume();
-////        Log.d(TAG, "onResume");
-//        //Update colleges as soon as they become available
-//        mHandler = new Handler();
-//        mHandler.postDelayed(mPopulateListViewRunnable, Constants.MILLISECONDS_BEFORE_POLLING);
-//    }
-//
-//    Runnable mPopulateListViewRunnable = new Runnable() {
-//        public void run() {
-//            //If data has not yet been downloaded, try again later
-//            if (mColleges == null || mColleges.size() == 0) {
-////                Log.d(TAG, "colleges still null");
-//                mHandler.postDelayed(this, Constants.MILLISECONDS_BETWEEN_POLLING);
-//                if (mProgressDialog == null) {
-//                    //Display loading spinner
-//                    mProgressDialog = new ProgressDialog(SelectCollegeActivity.this);
-//                    mProgressDialog.setMessage(getString(R.string.loading_message));
-//                    mProgressDialog.show();
-//                }
-//            }
-//            else {
-//                if (SelectCollegeActivity.this != null && mProgressDialog != null
-//                        && mProgressDialog.isShowing()) {
-//                    mProgressDialog.dismiss();
-//                    mProgressDialog = null;
-//                }
-//                mCustomBaseAdapter.setEntries(mColleges);
-//            }
-//        }
-//    };
-
+                                                @Override
+                                                public void onCancelled(DatabaseError databaseError) {
+                                                    Log.e(TAG, databaseError.getMessage());
+                                                }
+                                            }
+                                    );
+                        }
+                    }
+                });
+    }
 
     public void onBackPressed() {
         // Override to disable
